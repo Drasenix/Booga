6a5f4f87a5c9b7cf56ae9062a371f30be2558684
Drasenix <cauneauantoine@gmail.com>
2026-02-05 16:34:04 +0100

    feat: affichage accueil en fr

--- src/services/ServiceMenus.ts
+++ src/services/ServiceMenus.ts
@@ -1,69 +1,81 @@
-
 export class ServiceMenus {
+  private p5: any;
+  private tailleTexte: number;
 
-    private p5: any;
-    private tailleTexteGameOver: number;
-        
+  constructor(p5: any) {
+    this.p5 = p5;
+    this.tailleTexte = 100;
+  }
 
-    constructor(p5: any) {
-        this.p5 = p5;        
-        this.tailleTexteGameOver = 100;
-    }
+  drawCommencerPartie() {
+    this.p5.background(0, 1000);
 
-    drawCommencerPartie() {
-        this.p5.background(0, 1000);
+    this.p5.textSize(100);
+    this.p5.fill(255, 255, 255);
+    this.p5.text(
+      "Cliquez pour commencer !",
+      window.innerWidth / 2 - this.tailleTexte * 6,
+      window.innerHeight / 2,
+    );
+  }
 
-        this.p5.textSize(100);
-        this.p5.fill(255, 255, 255);
-        this.p5.text(
-            'Click to begin !',
-            window.innerWidth / 2 - 3 * this.tailleTexteGameOver,
-            window.innerHeight / 2
-        );        
-    }
+  drawPartiePerdue() {
+    this.p5.background(0, 1000);
 
-    drawPartiePerdue() {
-        this.p5.background(0, 1000);
+    this.p5.textSize(100);
+    this.p5.fill(255, 255, 255);
+    this.p5.text(
+      "Game Over !",
+      window.innerWidth / 2 - 3 * this.tailleTexte,
+      window.innerHeight / 2,
+    );
+    this.p5.text(
+      "Score : " +
+        this.p5.serviceControleurPartie
+          .getServiceScore()
+          .getScoreGlobal()
+          .getValeur(),
+      window.innerWidth / 2 - 3 * this.tailleTexte,
+      window.innerHeight / 2 + this.tailleTexte,
+    );
+    this.p5.text(
+      "Niveau : " +
+        this.p5.serviceControleurPartie
+          .getServiceNiveau()
+          .getNiveauActuel()
+          .getNumeroNiveau(),
+      window.innerWidth / 2 - 3 * this.tailleTexte,
+      window.innerHeight / 2 + 2 * this.tailleTexte,
+    );
+  }
 
-        this.p5.textSize(100);
-        this.p5.fill(255, 255, 255);
-        this.p5.text(
-            'Game Over !',
-            window.innerWidth / 2 - 3 * this.tailleTexteGameOver,
-            window.innerHeight / 2
-        );
-        this.p5.text(
-            'Score : ' + this.p5.serviceControleurPartie.getServiceScore().getScoreGlobal().getValeur(),
-            window.innerWidth / 2 - 3 * this.tailleTexteGameOver,
-            window.innerHeight / 2 + this.tailleTexteGameOver
-        );
-        this.p5.text(
-            'Niveau : ' + this.p5.serviceControleurPartie.getServiceNiveau().getNiveauActuel().getNumeroNiveau(),
-            window.innerWidth / 2 - 3 * this.tailleTexteGameOver,
-            window.innerHeight / 2 +  2 * this.tailleTexteGameOver
-        );
-    } 
-    
-    drawPartieGagnee() {
-        this.p5.background(0, 1000);
+  drawPartieGagnee() {
+    this.p5.background(0, 1000);
 
-        this.p5.textSize(100);
-        this.p5.fill(255, 255, 255);
-        this.p5.text(
-            'Bien joué !',
-            window.innerWidth / 2 - 3 * this.tailleTexteGameOver,
-            window.innerHeight / 2
-        );
-        this.p5.text(
-            'Score : ' + this.p5.serviceControleurPartie.getServiceScore().getScoreGlobal().getValeur(),
-            window.innerWidth / 2 - 3 * this.tailleTexteGameOver,
-            window.innerHeight / 2 + this.tailleTexteGameOver
-        );
-        this.p5.text(
-            'Niveau : ' + this.p5.serviceControleurPartie.getServiceNiveau().getNiveauActuel().getNumeroNiveau(),
-            window.innerWidth / 2 - 3 * this.tailleTexteGameOver,
-            window.innerHeight / 2 +  2 * this.tailleTexteGameOver
-        );
-    }
-
+    this.p5.textSize(100);
+    this.p5.fill(255, 255, 255);
+    this.p5.text(
+      "Bien joué !",
+      window.innerWidth / 2 - 3 * this.tailleTexte,
+      window.innerHeight / 2,
+    );
+    this.p5.text(
+      "Score : " +
+        this.p5.serviceControleurPartie
+          .getServiceScore()
+          .getScoreGlobal()
+          .getValeur(),
+      window.innerWidth / 2 - 3 * this.tailleTexte,
+      window.innerHeight / 2 + this.tailleTexte,
+    );
+    this.p5.text(
+      "Niveau : " +
+        this.p5.serviceControleurPartie
+          .getServiceNiveau()
+          .getNiveauActuel()
+          .getNumeroNiveau(),
+      window.innerWidth / 2 - 3 * this.tailleTexte,
+      window.innerHeight / 2 + 2 * this.tailleTexte,
+    );
+  }
 }
